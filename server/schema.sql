7b4ae579965fb5d3f8e9efa42d3b2e33e600db21
Matthew Rourke <mrourke@gmail.com>
2015-04-18 00:03:17 -0400

    DB shema design

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users(
+  id INT NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  name VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY(id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms(
+  id INT NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  name VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY(id)
+);
 
-
-
-
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
+CREATE TABLE messages(
+  id INT NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  text VARCHAR(140) NULL DEFAULT NULL,
+  userId INT,
+  roomId INT,
+  PRIMARY KEY(id),
+  INDEX(userId, roomId),
+  FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (roomId) REFERENCES rooms(id) ON DELETE CASCADE
+);
